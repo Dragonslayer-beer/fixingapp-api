644af9f5df5761a0ef459412c7a47a352eea47d5
Dragonslayer-beer <Aphisitbeerfc00@gmail.com>
2021-11-20 12:02:48 +0700

    OK

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(routes)
 app.use(passport.initialize())
 
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 9999
 
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
